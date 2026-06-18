da55c572ca489369bbbe2a24f59a88e49730a121
aldrin-samuel <aldrinfabinosamuel@gmail.com>
2026-06-18 08:19:41 +0530

    Added input to ask the no to be searched

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,5 +1,6 @@
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+input_value = int(input("Enter the element to search for: "))
 for i in range(len(array)):
-    if array[i] == 5:
+    if array[i] == input_value:
         print(f"Element found at index: {i}")
         break
